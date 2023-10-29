94a4351a0a0bdab8f1471b172610def126d3e514
Chamith Dilshan Ranathunga <73608973+ChamithDilshan@users.noreply.github.com>
2023-10-29 11:39:08 +0530

    Wavelet_properties done

--- Wavelet_transformation/wavelet_construction.m
+++ Wavelet_transformation/wavelet_construction.m
@@ -8,7 +8,7 @@ s = 0.01:0.1:2;     % Values of scaling factor
 % Maxican hat wavelet
 %...
 
-wavelt = 0.8671 .* exp(-0.5*t.^2).*(t.^2-1);
+wavelt = -0.8671 .* exp(-0.5*t.^2).*(t.^2-1);
 
 
 
@@ -16,29 +16,59 @@ figure;
 plot(t,wavelt,'b');
 xlabel("t (s)");
 ylabel("\psi(t)");
+title("Mother Wavelet");
+grid on;
 
-hold on;
-
-legend_text = cell(length(s)+1, 1);
-legend_text{1} = 'Original';
-
-for i = 1:length(s)
-    s_wavelt = (1/sqrt(s(i)))*wavelt;
-    plot(t, s_wavelt);
-    legend_text{i+1} = ['Scale Factor ' num2str(s(i))];
-    hold on;
+k = 1;
+for i = 1:5
+    figure;
+    for j = 1:4
+        subplot(1,4,j);
+        s_wavelt = (1/sqrt(s(k)))* (-0.8671 .* exp(-0.5*(t/s(k)).^2).*((t/s(k)).^2-1));
+        plot(t, s_wavelt);
+        ylim([-4,8.5]);
+        title_string = sprintf("Scaling factor = %.2f",s(k));
+        title(title_string);
+        grid on; 
+        disp(sprintf("When scaling factor is %.2f, mean = %.2f , energy = %.2f",s(k),mean(s_wavelt),round(trapz(t, wavelt.^2),2)));
+        k=k+1;
+    end 
 end
 
-legend(legend_text);
 
 
 
 
-% % Generating spectra of wavelets
-% Fwavelt = fft(wavelt)/length(wavelt);
-% hz = linspace(0,fs/2,floor(length(wavelt)/2)+1);
-% plot(hz,2*abs(Fwavelt(1:length(hz))))
-% xlabel('Frequency (Hz)'), ylabel('Amplitude')
+
+
+% Generating spectra of wavelets
+Fwavelt = fft(wavelt)/length(wavelt);
+hz = linspace(0,fs/2,floor(length(wavelt)/2)+1);
+figure;
+plot(hz,2*abs(Fwavelt(1:length(hz))));
+xlabel('Frequency (Hz)'), ylabel('Amplitude');
+title("Spectra of the Mother Wavelet")
+
+k = 1;
+for i = 1:5
+    figure;
+    for j = 1:4
+        subplot(1,4,j);
+        s_wavelt = (1/sqrt(s(k)))* (-0.8671 .* exp(-0.5*(t/s(k)).^2).*((t/s(k)).^2-1));
+
+        Fwavelt = fft(s_wavelt)/length(s_wavelt);
+        hz = linspace(0,fs/2,floor(length(s_wavelt)/2)+1);
+        
+        plot(hz,2*abs(Fwavelt(1:length(hz))));
+        xlabel('Frequency (Hz)'), ylabel('Amplitude');
+        ylim([0,0.2]);
+        title_string = sprintf("Scaling factor = %.2f",s(k));
+        title(title_string);
+        grid on; 
+        k=k+1;
+    end 
+end
+
 
 % % Ploting the spectrogram
 % h = pcolor();
